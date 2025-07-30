519aa4177f1fef797c9b1258b16aef423582b945
Mehdi Mansour <mehdi.mansor3115@gmail.com>
2025-07-30 17:26:49 +0100

    Updated Main js

--- main.js
+++ main.js
@@ -26,3 +26,66 @@ const winningConditions = [
   [2, 4, 6],
 
 ];
+  function updateBoard(index) {
+    board[index] = currentPlayer;
+  }
+  function renderCell(cell ,index) {
+    cells.textContent = board[index];
+  }
+  function makeMove(cell ,index) {
+
+    updateBoard(index);
+    renderCell(cell, index);
+  }
+function winningConditions.some(([a,b,c]) => {
+    return board[a] && board[a] === board[b] && board[a] === board[c];
+  })
+  function isDraw() {
+    return board.every((cell) => cell !== "");
+
+  }
+  function updateStatus() {
+      statusDiv.textContent = `It's ${currentPlayer}'s turn`;
+  }
+  function switchPlayer() {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+  }
+  function endGame(message) {
+    statusDiv.textContent = message;
+    gameActive = false;
+  }
+  function evaluateGame() {
+    if (winningMove) {
+      endGame(`Player ${currentPlayer} wins!`);
+    } else if (isDraw()) {
+      endGame("It's a draw!");
+    } else {
+      switchPlayer();
+    }
+  }
+  function isValidMove(index) {
+    return board[index] === "" && gameActive;
+  }
+  function handleCellClick(e) {
+    const clickedIndex   = parseInt(e.target.getAttribute("data-index"));
+    if (isValidMove(clickedIndex)) return;
+    makeMove(e.target, clickedIndex);
+    evaluateGame();
+    }
+    function resetGame() {
+      board = Array(9).fill("");
+      isGameActive = true;
+      currentPlayer = "X";
+      cells.forEach((cell) => (cell.textContent = ""));
+      updateStatus();
+    }
+    function attachEventListeners() {
+      cells.forEach((cell, index) => {
+        cell.addEventListener("click", handleCellClick);
+      resetBtn.addEventListener("click", resetGame);
+    } 
+    function  initializeGame() {
+      updateStatus();
+      attachEventListeners();
+    }
+    initializeGame();
